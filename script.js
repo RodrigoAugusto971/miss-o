6141a8bc502418d6153273fe2d753117f820a0ba
Rodrigo Augusto <108085684+RodrigoAugusto971@users.noreply.github.com>
2024-08-16 08:45:38 -0300

    misão

--- script.js
+++ script.js
@@ -6,23 +6,20 @@ const caixaPrincipal = document.querySelector (".texto-resultado");
 
 const perguntas = [
     {
-     enunciado:"Qual das seguintes linguagens de programação é mais comumente usada para desenvolvimento de aplicativos móveis nativos?"
+     enunciado:"Qual o impacto da poluição do ar na saúde de praticantes de esportes ao ar livre?"
      alternativas:[
-    "Java"
-    "Python"
-    "Swift"
+    
 
      ]
     }
     {
-    enunciado:""
-    alternativas:[
-
-    ]
+    texto:"A poluição pode aumentar o risco de doenças respiratórias e cardiovasculares"
+    afirmacao:["A exposição prolongada à poluição do ae pode diminuir a capacidade pulmonar, afetando o desempenho esportivo."
+ ]
     }
     {
-    enunciado:""
-    alternativas:[
+    texto:"A polução tem um efeito positivo na resistência física dos atletas."
+    afirmacao:["Atividades físicas ao ar livre em áreas poluídas podem aumentar o risco de problemas de saúde, especialmente em crianças e idosos"
 
     ]
     }
@@ -30,6 +27,7 @@ const perguntas = [
 
 let atual =0;
 let perguntaAtual;
+let historiaFinal="";
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
@@ -42,11 +40,19 @@ function mostraAlternativas(){
     for (const altwenativa of perguntaAtual.alternativas){
         const botaoAlternativas = documento.createElement("button");
         botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
 
     }
 
 } 
+
+ function respostaSelecionada(opcaoSlecionada){
+    const afirmacao = opcaoSlecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
+    mostraPergunta();
+ }
 
 mostraPergunta();
 
