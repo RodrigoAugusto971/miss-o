9aa58f6736ec3816209bdb338ffdd041129aba37
Rodrigo Augusto <108085684+RodrigoAugusto971@users.noreply.github.com>
2024-08-23 08:17:17 -0300

    missao

--- script.js
+++ script.js
@@ -6,57 +6,63 @@ const caixaPrincipal = document.querySelector (".texto-resultado");
 
 const perguntas = [
     {
-     enunciado:"Qual o impacto da poluição do ar na saúde de praticantes de esportes ao ar livre?"
+     enunciado:"Qual o impacto da poluição do ar na saúde de praticantes de esportes ao ar livre?",
      alternativas:[
     
 
      ]
-    }
+    },
     {
-    texto:"A poluição pode aumentar o risco de doenças respiratórias e cardiovasculares"
+    texto:"A poluição pode aumentar o risco de doenças respiratórias e cardiovasculares",
     afirmacao:["A exposição prolongada à poluição do ae pode diminuir a capacidade pulmonar, afetando o desempenho esportivo."
  ]
-    }
+    },
     {
     texto:"A polução tem um efeito positivo na resistência física dos atletas."
     afirmacao:["Atividades físicas ao ar livre em áreas poluídas podem aumentar o risco de problemas de saúde, especialmente em crianças e idosos"
 
     ]
-    }
+    },
 ];
 
-let atual =0;
+let atual = 0;
 let perguntaAtual;
-let historiaFinal="";
+let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for (const altwenativa of perguntaAtual.alternativas){
-        const botaoAlternativas = documento.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-
+        caixaAlternativas.appendChild(botaoAlternativas);        
     }
-
-} 
-
- function respostaSelecionada(opcaoSlecionada){
-    const afirmacao = opcaoSlecionada.afirmacao;
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
     historiaFinal = afirmacao;
     atual++
     mostraPergunta();
- }
+}
 
 mostraPergunta();
 
-
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
 
 
